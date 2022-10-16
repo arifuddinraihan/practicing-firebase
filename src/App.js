776b22b3b11ad64b67ca2f7bdd7811e76bf8b9f7
arifuddinraihan <mdarifuddinraihan@gmail.com>
2022-10-16 12:44:54 +0600

    added login and logout

--- src/App.js
+++ src/App.js
@@ -1,19 +1,38 @@
 import './App.css';
-import { getAuth, GoogleAuthProvider } from 'firebase/auth'
+import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth'
 import app from './Firebase/Firebase.init';
+import { useState } from 'react';
 
 const auth = getAuth(app);
-console.log(auth)
 
 function App() {
   const provider = new GoogleAuthProvider()
-  console.log(provider)
+  const [user , setUser] = useState({})
   const handleGoogleSign = () => {
-    console.log('Google sign in coming')
+    signInWithPopup(auth, provider)
+      .then(result => {
+        const user = result.user
+        setUser(user)
+      })
+      .catch(error => {
+        console.error("error :", error)
+      })
+  }
+  console.log(user)
+  const handleSignOut = () => {
+    signOut(auth)
+      .then()
+      .catch()
   }
   return (
     <div className="App">
       <button onClick={handleGoogleSign}>Google Sing In</button>
+      <button onClick={handleSignOut}>Sing Out</button>
+      <div>
+        <h3>User : {user.displayName}</h3>
+        <p>Email : {user.email}</p>
+        <img src={user.photoURL}></img>
+      </div>
     </div>
   );
 }
